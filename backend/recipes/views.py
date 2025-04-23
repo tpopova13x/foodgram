a819431b10791e82c00361b328a7c62c1b648e27
tpopova13x <33544859+tpopova13x@users.noreply.github.com>
2025-04-23 13:04:49 +0200

    Update views.py
    backend/recipes/views.py:200:44: E999 SyntaxError: unterminated string literal (detected at line 200)

--- backend/recipes/views.py
+++ backend/recipes/views.py
@@ -197,8 +197,8 @@ class RecipeViewSet(viewsets.ModelViewSet):
         response = HttpResponse(
             "\n".join(shopping_list),
             content_type="text/plain")
-        response["Content-Disposition"] = "attachment; 
-        filename=shopping_list.txt"
+        response["Content-Disposition"] = 
+                "attachment; filename=shopping_list.txt"
 
         return response
 
