75f3d773c8a26579f0802beff7097a60298d66d5
tpopova13x <33544859+tpopova13x@users.noreply.github.com>
2025-04-29 07:09:04 +0200

    fix path

--- backend/recipes/models.py
+++ backend/recipes/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth import get_user_model
 from django.core.validators import MinValueValidator, RegexValidator
 from django.db import models
 
-from backend.recipes.constants import MAX_LENGTH
+from recipes.constants import MAX_LENGTH
 
 User = get_user_model()
 
