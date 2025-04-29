e181bb8ea405dce78f1fbbf2b151454c03acff3b
tpopova13x <33544859+tpopova13x@users.noreply.github.com>
2025-04-29 07:40:38 +0200

    fix flake 8

--- backend/recipes/models.py
+++ backend/recipes/models.py
@@ -3,7 +3,6 @@
 from django.contrib.auth import get_user_model
 from django.core.validators import MinValueValidator, RegexValidator
 from django.db import models
-
 from recipes.constants import MAX_LENGTH
 
 User = get_user_model()
--- backend/users/views.py
+++ backend/users/views.py
@@ -5,7 +5,6 @@ from django.shortcuts import get_object_or_404
 from djoser.views import UserViewSet
 from rest_framework import status
 from rest_framework.decorators import action
-from rest_framework.exceptions import AuthenticationFailed, NotAuthenticated
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
